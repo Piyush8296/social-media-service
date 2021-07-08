c8705f8d65d421c185627400d42c968d9a6f6bc7
awiros <piyushbansal005@gmail.com>
2021-07-08 10:25:52 +0530

    1. Fetch list of comments added 2. Delete comments added

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -48,4 +48,47 @@ const handle_add_reaction = (req,res)=>{
 	})
 }
 
-module.exports = {handle_add_comment,handle_add_reaction}
+//assumption but not handled specifically:- user have his own single wall
+const handle_comments_fetch = (req,res)=>{
+	const entity_query = [
+		{"$match":{"soc_user":req.decoded["soc_id"], "soc_type":"parent"}}, //find all parent comments for a user
+		{
+		     $lookup:{
+		         from: "comments",
+		         localField: "soc_id",
+		         foreignField: "soc_parent_id",
+		         as: "soc_sub_comments"
+		    }
+	  	}
+	]
+
+
+	Comment.aggregate(entity_query)
+	.exec(function(err, info) {
+	  	if(info){
+	  		if (info.length != 0) {
+	  			res.json({success:true, msg:'Data found',data:info});
+		  	}else{
+				res.json({success:false, msg:'No data found'});
+		  	}
+		}else{
+			console.log(err)
+		  	res.json({success:false, msg:'Error fetching annotation data'});
+		}
+	});
+}
+
+const handle_comment_delete = (req,res)=>{
+	const find_query = { 
+		"$or":[
+			{ "soc_id":req.body["soc_id"] }, //for particular comment
+			{ "soc_parent_id":req.body["soc_id"] } // for child comments
+		] 
+	}
+
+	remove_entry_from_db("removing_comment",Comment,find_query,(response)=>{
+		res.json(response)
+	})
+}
+
+module.exports = {handle_add_comment,handle_add_reaction,handle_comments_fetch,handle_comment_delete}
--- routes/comments.js
+++ routes/comments.js
@@ -6,7 +6,7 @@ const config = require('../config');
 
 const {authentication} = require("../middlewares/authentication");
 const {check_if_parent_exists_for_sub_comment_with_provided_id} = require("../middlewares/common");
-const {handle_add_comment,handle_add_reaction} = require("../controllers/comment_controller")
+const {handle_add_comment,handle_add_reaction,handle_comments_fetch,handle_comment_delete} = require("../controllers/comment_controller")
 
 //either a comment or a sub comment
 router.use('/post_comment',authentication,check_if_parent_exists_for_sub_comment_with_provided_id);
@@ -20,5 +20,15 @@ router.post('/add_reaction', (req,res) => {
  	handle_add_reaction(req,res)
 })
 
+router.use('/list_of_comments_of_a_user',authentication);
+router.get('/list_of_comments_of_a_user', (req,res) => {
+ 	handle_comments_fetch(req,res)
+})
+
+//single API can be used to delete multiple as well, for now implementing single comment
+router.use('/single_comment',authentication);
+router.delete('/single_comment', (req,res) => {
+ 	handle_comment_delete(req,res)
+})
 
 module.exports = router;
